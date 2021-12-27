740096dde85aa75ef22be5dd935e9e5ea4e36030
JaafarRammal <jarammal@gmail.com>
2021-12-27 10:36:56 +0000

    impl

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ Steps:
 
 - `flutter create flutterwidget`
 - `cd flutterwidget`
+- If you add the packages, run `flutter pub get`
+- Open a device simulator and check it is connected with `flutter devices`
 - `flutter run`
--- flutterwidget/lib/main.dart
+++ flutterwidget/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_browser/flutter_web_browser.dart';
 
 void main() {
   runApp(const MyApp());
@@ -50,6 +51,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  openBrowserTab() async {
+    FlutterWebBrowser.openWebPage(
+      url: "https://widget.tryterra.co/session/9dcbd717-8651-4711-b68f-5bff9f5b2c23",
+      customTabsOptions: const CustomTabsOptions(
+        colorScheme: CustomTabsColorScheme.dark,
+        toolbarColor: Colors.deepPurple,
+        secondaryToolbarColor: Colors.green,
+        navigationBarColor: Colors.amber,
+        shareState: CustomTabsShareState.on,
+        instantAppsEnabled: true,
+        showTitle: true,
+        urlBarHidingEnabled: true,
+      ),
+      safariVCOptions: const SafariViewControllerOptions(
+        barCollapsingEnabled: true,
+        preferredBarTintColor: Colors.green,
+        preferredControlTintColor: Colors.amber,
+        dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+        modalPresentationCapturesStatusBarAppearance: true,
+      ),
+    );
+  }
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -75,34 +99,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: () => openBrowserTab(),
+          child: new Text('Open website'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
